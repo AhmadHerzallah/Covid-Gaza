b291adb9f440f53a767d6ecc14cc28cc15b48289
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2020-11-11 14:15:20 +0200

    .

--- src/components/Gaza.js
+++ src/components/Gaza.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Gaza() {
   return (
-    <div>
+    <div className="container">
       <h1>Here you can find all the data about Gaza Covid-19 Data</h1>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <h1>Hello!.</h1>
+      <p>Hello Gaza City Citizens and other people around the world!</p>
+      <p>
+        Here you can see Covid-19 data inside gaza, and outside! like Jenin and
+        other.
+      </p>
+      <p>Note: Sadly, we can't get Data from Jerusalem</p>
     </div>
   );
 }
